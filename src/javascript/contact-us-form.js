c7ac3590387e02e7b5c6c765d877becce204ff57
Mark22GitHub <atelezhenko93@gmail.com>
2022-12-06 19:42:24 +0200

    add all fixes from relevant bug lists for all pages

--- src/javascript/contact-us-form.js
+++ src/javascript/contact-us-form.js
@@ -87,34 +87,36 @@
 (function () {
   'use strict'
   // Fetch all the forms we need to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.requires-validation')
+  const forms = document.querySelectorAll('.needs-validation')
+  const emailInput = document.querySelector('.js-email-input')
+  const requiredInputError =  document.querySelector('.js-required-input-error')
+  const notValidEmailError =  document.querySelector('.js-not-valid-email-error')
+
   // Loop over them and prevent submission
   Array.from(forms)
     .forEach(function (form) {
       form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        } else {
-          // $('.toast').show();
+        event.preventDefault()
+        const isValidated = form.checkValidity()
+        requiredInputError.classList.add('d-none')
+        notValidEmailError.classList.add('d-none')
 
-          //   event.preventDefault();      //This stops page loading
-          //   $('.toast').show(); //Show toast
-          // setTimeout(() => {
-          //   form.reset();
-          // }, 2000);
+        if (isValidated) {
+          $('.toast').show();
+          form.reset()
+          form.classList.remove('was-validated')
+        } else {
+          const emailValue = emailInput.value
+
+          form.classList.add('was-validated')
+
+          if (!emailValue) {
+            requiredInputError.classList.remove('d-none')
+          } else {
+            notValidEmailError.classList.remove('d-none')
+          }
         }
 
-        // function showSuccess() {
-        //   $('.toast').show();
-        // }
-
-        // if (form.checkValidity()) {
-        //   $('.toast').show();
-        //   // setTimeout(showSuccess, 2000);
-        // }
-
-        form.classList.add('was-validated')
       }, false)
     })
 })()
--- src/javascript/particles-bitcoin.js
+++ src/javascript/particles-bitcoin.js
@@ -3,115 +3,117 @@ window.addEventListener('DOMContentLoaded', (event) => {
   'use strict';
 
   /* ---- particles.js config ---- */
-  particlesJS("particles-js", {
-    particles: {
-      number: {
-        value: 7,
-        density: {
-          enable: false,
-          value_area: 600
-        }
-      },
-      color: {
-        value: "#ffffff"
-      },
-      shape: {
-        type: "image",
-        stroke: {
-          width: 0,
-          color: "#000000"
-        },
-        polygon: {
-          nb_sides: 5
-        },
-        image: {
-          src: "/bitcoin.png",
-          width: 100,
-          height: 100,
-        }
-      },
-      opacity: {
-        value: 0.1,
-        random: false,
-        anim: {
-          enable: true,
-          speed: 1,
-          opacity_min: 0.1,
-          sync: false
-        }
-      },
-      size: {
-        value: 20,
-        random: false,
-        anim: {
-          enable: false,
-          speed: 40,
-          size_min: 50,
-          sync: false
-        }
-      },
-      line_linked: {
-        enable: false,
-        distance: 200,
-        color: "#ffffff",
-        opacity: 0.4,
-        width: 1
-      },
-      move: {
-        enable: true,
-        speed: 2,
-        direction: "none",
-        random: true,
-        straight: false,
-        out_mode: "bounce",
-        bounce: false,
-        attract: {
-          enable: false,
-          rotateX: 80,
-          rotateY: 80
-        }
-      }
-    },
-    interactivity: {
-      detect_on: "canvas",
-      events: {
-        onhover: {
-          enable: false,
-          mode: "repulse"
-        },
-        onclick: {
-          enable: false,
-          mode: "repulse"
-        },
-        resize: true
-      },
-      modes: {
-        grab: {
-          distance: 200,
-          line_linked: {
-            opacity: 0.5017974219129172
+  setTimeout(() => {
+    particlesJS("particles-js", {
+      particles: {
+        number: {
+          value: 7,
+          density: {
+            enable: false,
+            value_area: 600
           }
         },
-        bubble: {
-          distance: 400,
-          size: 40,
-          duration: 2,
-          opacity: 8,
-          speed: 3
+        color: {
+          value: "#ffffff"
         },
-        repulse: {
+        shape: {
+          type: "image",
+          stroke: {
+            width: 0,
+            color: "#000000"
+          },
+          polygon: {
+            nb_sides: 5
+          },
+          image: {
+            src: "/bitcoin.png",
+            width: 100,
+            height: 100,
+          }
+        },
+        opacity: {
+          value: 0.1,
+          random: false,
+          anim: {
+            enable: true,
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false
+          }
+        },
+        size: {
+          value: 20,
+          random: false,
+          anim: {
+            enable: false,
+            speed: 40,
+            size_min: 50,
+            sync: false
+          }
+        },
+        line_linked: {
+          enable: false,
           distance: 200,
-          duration: 0.4
+          color: "#ffffff",
+          opacity: 0.4,
+          width: 1
         },
-        push: {
-          particles_nb: 4
-        },
-        remove: {
-          particles_nb: 2
+        move: {
+          enable: true,
+          speed: 2,
+          direction: "none",
+          random: true,
+          straight: false,
+          out_mode: "bounce",
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 80,
+            rotateY: 80
+          }
         }
-      }
-    },
-    retina_detect: true
-  });
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onhover: {
+            enable: false,
+            mode: "repulse"
+          },
+          onclick: {
+            enable: false,
+            mode: "repulse"
+          },
+          resize: true
+        },
+        modes: {
+          grab: {
+            distance: 200,
+            line_linked: {
+              opacity: 0.5017974219129172
+            }
+          },
+          bubble: {
+            distance: 400,
+            size: 40,
+            duration: 2,
+            opacity: 8,
+            speed: 3
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4
+          },
+          push: {
+            particles_nb: 4
+          },
+          remove: {
+            particles_nb: 2
+          }
+        }
+      },
+      retina_detect: true
+    });
+  }, 1000)
 
 });
