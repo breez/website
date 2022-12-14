9ed9a7f83b4b7d65fb46009804aeaa9d49ef55b8
Mark22GitHub <atelezhenko93@gmail.com>
2022-12-14 10:28:59 +0200

    make hero section fixes

--- src/index.js
+++ src/index.js
@@ -31,6 +31,28 @@ import './javascript/particles-bitcoin'
 
 import { load } from 'recaptcha-v3'
 
+
+const reCalc = () => {
+    if(window.innerWidth <= 576) {
+        const heroHeight = $('.hero-section').height()
+        // const heroHeight = $(window).height();
+        const infoHeight = $('#hero-info-block').height()
+
+        const imageHeight = (heroHeight - infoHeight - 122) / 2
+        const imgContainer = $('.image-container')
+
+        imgContainer.css('height', imageHeight+'px')
+
+        console.log(heroHeight, 'heroHeight');
+        console.log(infoHeight, 'infoHeight');
+        console.log(imageHeight, 'imageHeight');
+    }
+}
+
+reCalc()
+
+
+
 load('6LcXRpgUAAAAAME7hVWwxUI0BvOuz6QEocYmUpIa').then((recaptcha) => {
     recaptcha.execute('contact').then((token) => {
         var recaptchaResponse = document.getElementById('recaptcha_response');
