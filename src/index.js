8b6cde488d1f701dc19bb1917193e3750502ed5d
Mark22GitHub <atelezhenko93@gmail.com>
2022-12-16 12:41:29 +0200

    fix hero section paddings, change arrow position, optimize home page, change and improve preloader, add logic to remove preloader when recalc has been finished

--- src/index.js
+++ src/index.js
@@ -34,6 +34,36 @@ import './javascript/spinner-loader'
 import { load } from 'recaptcha-v3'
 
 
+// /////// decrease time loader
+//
+// /**
+//  * Trigger a callback when the selected images are loaded:
+//  * @param {String} selector
+//  * @param {Function} callback
+//  */
+//
+// const spinnerWrapperEl = document.querySelector('.spinner-wrapper');
+//
+// const onImgLoad = function(selector, callback){
+//     $(selector).each(function(){
+//         if (this.complete || /*for IE 10-*/ $(this).height() > 0) {
+//             callback.apply(this);
+//         }
+//         else {
+//             $(this).on('load', function(){
+//                 callback.apply(this);
+//             });
+//         }
+//     });
+// };
+//
+// onImgLoad('img', function(){
+//     console.log('remove loader');
+//     spinnerWrapperEl.style.display = 'none';
+// });
+
+
+
 load('6LcXRpgUAAAAAME7hVWwxUI0BvOuz6QEocYmUpIa').then((recaptcha) => {
     recaptcha.execute('contact').then((token) => {
         var recaptchaResponse = document.getElementById('recaptcha_response');
