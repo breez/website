7e711c5c0c1e7165fb36d2a9ab52e9c106440693
Yaacov Akiba Slama <yaslama@breez.technology>
2024-11-13 10:15:53 +0200

    Use the term Nodeless instead of Liquid

--- contact_handler/main.go
+++ contact_handler/main.go
@@ -88,7 +88,7 @@ func sendAPIKey(c contact) error {
 
 	tpl := `
 Hello {{ .FullName }},<br /><br />
-Please use the following API key for the Breez SDK (Liquid implementation):<br /><br />
+Please use the following API key for the Breez SDK (Nodeless implementation):<br /><br />
 <pre>{{ .APIKey }}</pre><br />
 If you have any questions, you can reply to this email.<br />
 <br />
